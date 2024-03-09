1baf79392656fb879a08940df0225c1781e2a3c9
hubin <hubin73@huawei.com>
2024-03-09 23:43:07 +0800

    update install script for benchbase

--- scripts/install_benchbase.sh
+++ scripts/install_benchbase.sh
@@ -1,6 +1,7 @@
-sudo apt-get update
-sudo apt-get install git
-git clone --depth 1 https://github.com/cmu-db/benchbase.git ../benchbase
+# sudo apt-get update
+# sudo apt-get install git
+# git clone --depth 1 https://github.com/cmu-db/benchbase.git ../benchbase
+git clone --depth 1 https://github.com/he7850/benchbase.git ../benchbase
 cd ../benchbase
 ./mvnw clean package -P $1
 cd target
